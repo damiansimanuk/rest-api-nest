aaa0c6321bb38025a0bcd226a5779ed9e7581ddf
Damián Simañuk <damiansimanuk@gmail.com>
2021-08-21 11:13:56 -0300

    add logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from "@nestjs/common"
 import { AppService } from "./app.service"
+import { LoggerService } from "./logger/logger.service"
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly log: LoggerService) {
+    log.setContext(AppController.name)
+  }
 
   @Get()
   getHello(): string {
+    this.log.error("mi abuela")
     return this.appService.getHello()
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import { LoggerService } from "./logger/logger.service"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const app = await NestFactory.create(AppModule, {
+    logger: new LoggerService()
+  })
+  app.useLogger(await app.resolve(LoggerService))
+
+  const logger = await app.resolve(LoggerService)
+  logger.setContext(bootstrap.name)
+
+  const PORT = 3000
+  await app.listen(PORT)
+  logger.log(`App Started. Listen port:${PORT}`)
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
